cb226b0f7b2b8ae10ceb2445a17020129774c9f4
ZbulwersowanyBogdan <65741183+ZbulwersowanyBogdan@users.noreply.github.com>
2020-05-25 23:43:15 +0200

    Update allbash.sh

--- allbash.sh
+++ allbash.sh
@@ -6,3 +6,4 @@ sudo dpkg -i docker-ce_18.06.0~ce~3-0~ubuntu_amd64.deb
 sudo docker version
 sudo docker pull jenkins/jenkins:lts
 docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+#then jenkins passwd appears and i proceed to jenkins GUI
