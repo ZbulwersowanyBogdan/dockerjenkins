44840a27284139de57e67d3000748b50a91fd34a
ZbulwersowanyBogdan <65741183+ZbulwersowanyBogdan@users.noreply.github.com>
2020-05-25 22:35:14 +0200

    Update allbash.sh

--- allbash.sh
+++ allbash.sh
@@ -4,3 +4,4 @@ sudo apt upgrade
 wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_18.06.0~ce~3-0~ubuntu_amd64.deb
 sudo dpkg -i docker-ce_18.06.0~ce~3-0~ubuntu_amd64.deb
 sudo docker version
+sudo docker pull jenkins/jenkins:lts
