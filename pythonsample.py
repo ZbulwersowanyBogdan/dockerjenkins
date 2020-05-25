21a3fdbd19b870005047b96f4f2888af51a9e908
ZbulwersowanyBogdan <65741183+ZbulwersowanyBogdan@users.noreply.github.com>
2020-05-25 23:45:46 +0200

    Update pythonsample.py

--- pythonsample.py
+++ pythonsample.py
@@ -1,1 +1,8 @@
-print("hello darkness")
+import os
+import multiprocessing
+print('\n' * 8)
+print('liczba rdzeni procesora: ', multiprocessing.cpu_count())
+print('\n' * 2)
+mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  
+mem_gib = mem_bytes/(1024.**3)
+print('ilosc pamieci ram maszyny wirtualnej', mem_gib)
