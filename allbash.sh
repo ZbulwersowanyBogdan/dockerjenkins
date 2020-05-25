d20f3ed4da1ced3a62be0d6115b5ffd5a81271af
ZbulwersowanyBogdan <65741183+ZbulwersowanyBogdan@users.noreply.github.com>
2020-05-25 23:40:18 +0200

    Update allbash.sh

--- allbash.sh
+++ allbash.sh
@@ -5,3 +5,4 @@ wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/doc
 sudo dpkg -i docker-ce_18.06.0~ce~3-0~ubuntu_amd64.deb
 sudo docker version
 sudo docker pull jenkins/jenkins:lts
+docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
