a3f38bfba6f86fc5f82e69aa114d4371c801f2d3
ZbulwersowanyBogdan <65741183+ZbulwersowanyBogdan@users.noreply.github.com>
2020-05-26 18:33:16 +0200

    Update sysinfo.py

--- sysinfo.py
+++ sysinfo.py
@@ -3,7 +3,15 @@ import os
 import multiprocessing
 print('\n' * 8)
 print('liczba rdzeni procesora: ', multiprocessing.cpu_count())
+
+
+from psutil import virtual_memory
+
+mem = virtual_memory()
+mem.total
+mem_gib = (mem.total/1024/1024)
+
+
 print('\n' * 2)
-mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  
-mem_gib = mem_bytes/(1024.**3)
+
 print('ilosc pamieci ram maszyny wirtualnej', mem_gib)
