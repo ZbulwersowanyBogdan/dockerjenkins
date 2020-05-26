6a960848d2c8decb72b459d2bd6471166e3c02ce
ZbulwersowanyBogdan <65741183+ZbulwersowanyBogdan@users.noreply.github.com>
2020-05-26 19:24:37 +0200

    Update mem.py

--- mem.py
+++ mem.py
@@ -1,5 +1,11 @@
-from psutil import virtual_memory
-
-mem = virtual_memory()
-mem.total  # total physical memory available
-print(mem.total)
+import os 
+  
+# System Configuration variable 
+name = "SC_PAGE_SIZE"
+  
+# Get the integer-valued  
+# configuration value corresponding 
+# to the specified configuration  
+# variable using os.sysconf() method 
+value = int(os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / (1024.0**2))
+print(value)
